d32a6887d308f06184a7a387f496e460778de519
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-03-09 16:33:19 -0400

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,7 @@
+document.getElementById("blueTheme").onclick = () => {
+  document.documentElement.className = "theme-blue";
+};
+
+document.getElementById("redTheme").onclick = () => {
+  document.documentElement.className = "theme-red";
+};
